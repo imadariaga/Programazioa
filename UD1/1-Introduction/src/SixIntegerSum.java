3d923bbc85e7411f1e0c0970ba2d4c04621cad57
hidalgosandra <114407178+hidalgosandra@users.noreply.github.com>
2022-09-27 16:50:01 +0200

    Update SixIntegerSum.java

--- UD1/1-Introduction/src/SixIntegerSum.java
+++ UD1/1-Introduction/src/SixIntegerSum.java
@@ -1,7 +1,6 @@
 public SixIntegerSum Main {
     public static void main(Strings[] args) {
         int number6 = 66;
-        int number5 = 55;
         int sum;
         sum = number5 + number6;
         System.out.print("the sum is");
